49ab2b2b35707f53af91498e30f473818ee8e5ce
Firerat <firer4t@googlemail.com>
2011-01-13 10:49:53 +0000

    init.d script to determine if system dex can fit on /cache will probably simplify it in the future

--- device_dream_sapphire.mk
+++ device_dream_sapphire.mk
@@ -64,6 +64,7 @@ PRODUCT_COPY_FILES += \
     device/htc/dream-sapphire/prebuilt/bin/sysinit.rc:/system/bin/sysinit.rc \
     device/htc/dream-sapphire/prebuilt/etc/init.d/00banner:/system/etc/init.d/00banner \
     device/htc/dream-sapphire/prebuilt/etc/init.d/03firstboot:/system/etc/init.d/03firstboot \
+    device/htc/dream-sapphire/prebuilt/etc/init.d/04sys_dex_loc:/system/etc/init.d/04sys_dex_loc \
     device/htc/dream-sapphire/prebuilt/etc/init.d/05mountsd:/system/etc/init.d/05mountsd \
     device/htc/dream-sapphire/prebuilt/etc/init.d/10apps2sd:/system/etc/init.d/10apps2sd \
     device/htc/dream-sapphire/prebuilt/etc/init.d/20userinit:/system/etc/init.d/20userinit
