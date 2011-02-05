480d7fe05d2fd4679c483fbbd9bf7843c33551c8
Firerat <firer4t@googlemail.com>
2011-02-05 22:04:54 +0000

    new script to setup environment two functions system_dex_loc , this determines if system's dalvik-cache can fit on /cache download_cache , this sets download_cache location requires:     https://github.com/Firerat/GB-framework_base/commit/40ca809a5dffebacb2b9dc7d466775cea30cbd49

--- device_dream_sapphire.mk
+++ device_dream_sapphire.mk
@@ -81,8 +81,8 @@ PRODUCT_COPY_FILES += \
     device/htc/dream-sapphire/prebuilt/etc/init.d/00banner:/system/etc/init.d/00banner \
     device/htc/dream-sapphire/prebuilt/etc/init.d/03firstboot:/system/etc/init.d/03firstboot \
     device/htc/dream-sapphire/prebuilt/etc/init.d/04modules:/system/etc/init.d/04modules \
-    device/htc/dream-sapphire/prebuilt/etc/init.d/04sys_dex_loc:/system/etc/init.d/04sys_dex_loc \
     device/htc/dream-sapphire/prebuilt/etc/init.d/05mountsd:/system/etc/init.d/05mountsd \
+    device/htc/dream-sapphire/prebuilt/etc/init.d/06SetEnv:/system/etc/init.d/06SetEnv \
     device/htc/dream-sapphire/prebuilt/etc/init.d/06BindCache:/system/etc/init.d/06BindCache \
     device/htc/dream-sapphire/prebuilt/etc/init.d/10apps2sd:/system/etc/init.d/10apps2sd \
     device/htc/dream-sapphire/prebuilt/etc/init.d/20userinit:/system/etc/init.d/20userinit
