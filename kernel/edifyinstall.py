91d31e98be36d91f6debcd79911576e6e8a24817
AndDiSa <dsagwitz@web.de>
2011-08-28 15:56:39 +0200

    take terry's version

--- kernel/edifyinstall.py
+++ kernel/edifyinstall.py
@@ -91,30 +91,6 @@ echo "sysboard=$board" >> /tmp/nfo.prop
 """
     common.ZipWriteStr(output_zip,"checksys.sh",checksys)
 
-def generate_repackboot(output_zip):
-    repackboot  = \
-"""#!/sbin/sh
-# customMTD boot only patcher
-# get mtdparts from running recovery and patch boot.img with it
-# Firerat 2011-01-17
-KCMDline="mtdparts`cat /proc/cmdline|awk -Fmtdparts '{print $2}'`"
-if [ "$KCMDline" = "mtdparts" ];
-then
-    # we shouldn't be running this script if no customMTD
-    # but just in case exit here if no mtdparts found
-    exit
-fi
-/tmp/unpackbootimg /tmp/boot.img /tmp/
-origcmdline=`awk '{gsub(/\ .\ /,"");sub(/mtdparts.+)/,"");print}' /tmp/boot.img-cmdline|awk '{$1=$1};1'`
-/tmp/mkbootimg --kernel /tmp/boot.img-zImage \
-               --ramdisk /tmp/boot.img-ramdisk.gz \
-               --cmdline "$origcmdline $KCMDline" \
-               --base `cat /tmp/boot.img-base` \
-               -o /tmp/boot.img
-
-rm /tmp/boot.img-*
-"""
-    common.ZipWriteStr(output_zip,"AutoCMTD/repackboot.sh",repackboot)
 
 def make_boot_install(script,boot_img,input_zip,output_zip):
     android_root=os.getenv("ANDROID_BUILD_TOP")
@@ -247,7 +223,7 @@ endif;
 
 ui_print("Write boot.img");
 assert(write_raw_image("/tmp/boot.img","boot"));
-delete("/tmp/checksys.sh","/tmp/repackboot.sh","/tmp/boot.img");
+delete("/tmp/checksys.sh","/tmp/boot.img");
 
 #END INSTALL boot.img
 
