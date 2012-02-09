9ea18eaa5f7dd2fbe8b8176c678aad27e8ed868a
AndDiSa <anddisa@googlemail.com>
2012-02-09 18:22:53 +0100

    change call of sysinit

--- device_dream_sapphire.mk
+++ device_dream_sapphire.mk
@@ -51,6 +51,7 @@ PRODUCT_COPY_FILES += \
 #Copy prebuilt files
 PRODUCT_COPY_FILES += \
     device/htc/dream-sapphire/prebuilt/bin/fix_permissions:system/bin/fix_permissions \
+    device/htc/dream-sapphire/prebuilt/bin/sysinit:system/bin/sysinit \
     device/htc/dream-sapphire/prebuilt/xbin/update_ksm:system/xbin/update_ksm \
     device/htc/dream-sapphire/prebuilt/etc/init.local.rc:system/etc/init.local.rc \
     device/htc/dream-sapphire/prebuilt/build.trout.prop:system/build.trout.prop 
